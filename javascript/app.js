34b3daaea69b431e62b87b9cf7a6fec392c8bdb3
Rodrigo Simão <rodrigoip435@gmail.com>
2024-11-08 17:51:37 -0300

    concluido

--- javascript/app.js
+++ javascript/app.js
@@ -4,3 +4,22 @@ const icons = document.getElementById("icons");
 menuButton.addEventListener("click", () => {
   icons.classList.toggle("show");
 });
+
+const form = document.querySelector("form");
+
+function validarFormulario(event) {
+  event.preventDefault();
+
+  const nome = document.getElementById("nome");
+  const email = document.getElementById("email");
+  const mensagem = document.getElementById("mensagem");
+
+  // Verifica se todos os campos estão preenchidos
+  if (nome.value !== "" && email.value !== "" && mensagem.value !== "") {
+    alert("E-mail enviado com sucesso!"); // Mensagem de sucesso
+    form.reset(); // Limpa o formulário, caso deseje
+  }
+}
+
+// Escutando o evento de envio do formulário
+form.addEventListener("submit", validarFormulario);
